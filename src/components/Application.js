c351fb1b97f66f65b591034428d96c0e0f90c809
daryl <tsdaryl@gmail.com>
2021-02-16 22:03:07 -0800

    complete day2

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "components/Application.scss";
-import Button from "components/Button";
-import DayListItem from "components/DayListItem";
+// import Button from "components/Button";
+// import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import Appointment from 'components/Appointment';
 
 
 const days = [
@@ -23,35 +24,101 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Mike Rave Owen",
+      interviewer: {
+        id: 1,
+        name: "Andy",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Manpreet",
+      interviewer: {
+        id: 1,
+        name: "Bradley",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Appointment guy",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }
+];
+
+
 export default function Application(props) {
 
   const [selectedDay, setSelectedDay] = useState('Monday');
 
+  const formatedAppointments = appointments.map((item) => {
+    return (
+      <Appointment
+        key={item.id}
+        {...item}
+      />
+    );
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  key={days.id}
-  days={days}
-  day={"Monday"}
-  setDay={day => console.log(day)}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+        <DayList
+          key={days.id}
+          days={days}
+          day={"Monday"}
+          setDay={day => console.log(day)}
+        />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-      
+        { formatedAppointments }
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
