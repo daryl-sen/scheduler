1ae883fd24506332ee9f7732437744b2e389d9b6
daryl <tsdaryl@gmail.com>
2021-02-24 16:14:30 -0800

    complete cypress test for navigating days

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,15 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+
+  xit("should visit root", () => {
     cy.visit("/");
+  });
+
+  it('should navigate to Tuesday', () => {
+    // 1. Visit root of webserver
+    cy.visit('/');
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
