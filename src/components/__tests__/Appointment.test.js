4225a76e9a2ede7a1080c49d84f81c9c6d9114d2
daryl <tsdaryl@gmail.com>
2021-02-24 18:28:21 -0800

    enable all tests

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Application from "components/Application";
 
-it.skip("renders without crashing", () => {
+it("renders without crashing", () => {
   render(<Application />);
 });
