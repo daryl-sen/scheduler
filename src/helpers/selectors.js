78e63fd1ffd05a694269f2c019d5107563b4cd05
daryl <tsdaryl@gmail.com>
2021-03-01 14:36:50 -0800

    change selectors.js to use .map instead of array.push

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,14 +1,13 @@
 export const getAppointmentsForDay = function(state, day) {
-  let output = [];
+
   const selectedDay = state.days.filter((dayObj) => dayObj.name === day);
   if (selectedDay.length === 0) {
     return [];
   }
   const selectedDayAppointmentIDs = selectedDay[0].appointments;
 
-  for (const id of selectedDayAppointmentIDs) {
-    output.push(state.appointments[id]);
-  }
+  let output = selectedDayAppointmentIDs.map(id => state.appointments[id]);
+  console.log(output);
   return output;
 };
 
